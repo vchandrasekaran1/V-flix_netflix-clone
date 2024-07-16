823b3af8f92fce8c5b87001c2d57124aec80a777
Viraj Chandrasekaran <legospy44@gmail.com>
2024-07-16 18:29:38 -0400

    added info modal functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ import MovieList from "@/components/MovieList";
 
 import useMovieList from "@/hooks/useMovieList";
 import useFavorites from "@/hooks/useFavorites";
+import InfoModal from "@/components/InfoModal";
+import useInfoModal from "@/hooks/useInfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -32,15 +34,17 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
-    <Navbar />
-    <Billboard />
-    <div className="pb-40">
-      <MovieList title="Trending Now" data={movies} />
-      <MovieList title="My List" data={favorites} />
-    </div>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
     </>
   );
 }
